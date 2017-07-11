b6480958c69004ee91307c84c50301d5d0c14a9c
Aaron <aaronrod@gmail.com>
2017-07-11 11:32:37 +0100

    Adds test for checking if account balance is zero

--- lib/bankaccount.rb
+++ lib/bankaccount.rb
@@ -1,3 +1,8 @@
 class BankAccount
 
+  attr_reader :balance
+
+  def initialize(balance = 0)
+    @balance = balance
+  end
 end
--- spec/bankaccount_spec.rb
+++ spec/bankaccount_spec.rb
@@ -7,4 +7,8 @@ describe BankAccount do
   it 'should exist' do
     expect(account).to be_truthy
   end
+
+  it 'should have a balance of zero when created' do
+    expect(account.balance).to eq 0
+  end
 end
