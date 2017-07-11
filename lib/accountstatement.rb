44179a742027daca41089f175392497be40b913f
Aaron <aaronrod@gmail.com>
2017-07-11 13:55:23 +0100

    Adds test to print out header

--- lib/accountstatement.rb
+++ lib/accountstatement.rb
@@ -1,3 +1,6 @@
 class AccountStatement
-  
+
+  def print_out_account_statement_header
+    puts 'date || credit || debit || balance'
+  end
 end
--- spec/accountstatement_spec.rb
+++ spec/accountstatement_spec.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/kernel/reporting'
 require 'accountstatement'
 
 describe AccountStatement do
@@ -6,5 +7,9 @@ describe AccountStatement do
 
   it 'should exist' do
     expect(statement).to be_truthy
+  end
+
+  it 'should print out a header for the statement' do
+    expect{statement.print_out_account_statement_header}.to output("date || credit || debit || balance" + "\n").to_stdout
   end
 end
